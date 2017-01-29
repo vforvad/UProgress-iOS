16cbd6779f254baa967453f35008a0e7365a33ca
Vadim Sokoltsov <vforvad@gmail.com>
2017-01-29 19:38:44 +0300

    move directions list table view into separated class

---
+++ UProgress/DirectionsListView.swift
@@ -0,0 +1,43 @@
+//
+//  DirectionsListView.swift
+//  UProgress
+//
+//  Created by Vadim Sokoltsov on 29.01.17.
+//  Copyright © 2017 vsokoltsov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DirectionsListView: NSObject, UITableViewDataSource, UITableViewDelegate {
+    var cellIdentifier = "cellId"
+    var itemsList:[Direction]! = []
+    private var tableView: UITableView!
+    private var searchBar: UISearchBar!
+    private var viewController: DirectionsListViewProtocol!
+    
+    init(viewController: DirectionsListViewProtocol!, table: UITableView!, searchBar: UISearchBar!) {
+        super.init()
+        self.viewController = viewController
+        self.tableView = table
+        self.searchBar = searchBar
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "cellId")
+        cell.textLabel?.text = itemsList[indexPath.row].title
+        return cell
+    }
+
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsList.count
+    }
+    
+    public func updateData(directions: [Direction]!) {
+        self.itemsList = directions
+        self.tableView.reloadData()
+    }
+}
