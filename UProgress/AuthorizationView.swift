27f892e5c2abc3373d37777a355d358e58d6dc16
Vadim Sokoltsov <vforvad@gmail.com>
2017-02-01 14:19:58 +0300

    add prototype for uiscrollview in authorizations controller

---
+++ UProgress/AuthorizationView.swift
@@ -0,0 +1,14 @@
+//
+//  AuthorizationView.swift
+//  UProgress
+//
+//  Created by Vadim Sokoltsov on 31.01.17.
+//  Copyright © 2017 vsokoltsov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AuthorizationView: NSObject {
+
+}
--- UProgress/AuthorizationsViewController.swift
+++ UProgress/AuthorizationsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class AuthorizationsViewController: BaseViewController {
     public var signIn: Bool!
