532f5e8f721518be61f5ba5ded247f11a6025622
Vadim Sokoltsov <vforvad@gmail.com>
2017-02-01 18:53:50 +0300

    implement success authorization

--- UProgress/AuthorizationManager.swift
+++ UProgress/AuthorizationManager.swift
@@ -19,14 +19,23 @@ class AuthorizationManager: AuthorizationManagerProtocol {
     }
     
     internal func signIn(signInParameters: Dictionary<String, AnyObject>, success: @escaping (String) -> Void, failure: @escaping (NSError) -> Void) {
-        ApiRequest.sharedInstance.post(url: "/sessions", parameters: ["user": signInParameters] as NSDictionary).responseJSON { response in
+        var params = signInParameters
+        params.updateValue(self.deviseInormation(provider: "UProgress"), forKey: "authorization")
+        ApiRequest.sharedInstance.post(url: "/sessions", parameters: ["user": params] as NSDictionary).responseJSON { response in
             switch(response.result) {
             case .success(let value):
-                let dictionary = value as! Dictionary<String, String>
-                success(dictionary["token"]!)
+                let dictionary = value as! Dictionary<String, AnyObject>
+                success(dictionary["token"]! as! String)
             case .failure(let errorValue):
                 failure(errorValue as NSError)
             }
         }
+    }
+    
+    func deviseInormation(provider: String!) -> NSDictionary {
+        let devise = UIDevice.current
+        let deviseData = ["platform": devise.systemName, "platform_version": devise.systemVersion,
+                          "app_name": devise.name, provider: provider]
+        return deviseData as NSDictionary
     }
 }
