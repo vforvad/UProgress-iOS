9ee3c6786a8ff71a8c9df1d923a3750202bfbfde
Vadim Sokoltsov <vforvad@gmail.com>
2017-03-05 16:59:16 +0300

    Fix another strange issue

--- UProgressUITests/UProgressUITests.swift
+++ UProgressUITests/UProgressUITests.swift
@@ -11,8 +11,7 @@ import XCTest
 class UProgressUITests: XCTestCase {
         
     override func setUp() {
-        super.setUp()
-        
+        super.setUp()
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
