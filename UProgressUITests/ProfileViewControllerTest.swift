a8354cce90962278e196bac45ce9bfa5ce6efa69
Vadim Sokoltsov <vforvad@gmail.com>
2017-03-15 14:48:15 +0300

    Add description of ui tests in README.md

---
+++ UProgressUITests/ProfileViewControllerTest.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewControllerTest.swift
+//  UProgress
+//
+//  Created by Vadim Sokoltsov on 15.03.17.
+//  Copyright © 2017 vsokoltsov. All rights reserved.
+//
+
+import Foundation
